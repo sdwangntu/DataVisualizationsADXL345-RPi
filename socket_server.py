71e285d902b0fc82c8785f4b42e4e82a90e61a28
Sheng-De Wang <sdwang@ntu.edu.tw>
2019-11-21 08:22:17 +0800

    Update and rename socket_server_adxl345.py to socket_server.py

--- socket_server.py
+++ socket_server.py
@@ -7,8 +7,8 @@ import json
 import time
 import random
 
-HOST = 'localhost'  # Standard loopback interface address (localhost)
-PORT = 65431            # Port to listen on (non-privileged ports are > 1023)
+HOST = '??????' # sensor device's IP or hostname (localhost)
+PORT = 65431    # Port to listen on (non-privileged ports are > 1023)
 
 sample = 0
 
